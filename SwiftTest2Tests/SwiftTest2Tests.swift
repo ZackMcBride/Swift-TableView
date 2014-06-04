2657f4931a22583fd97d1239aeea05ed9ea67643
Zack McBride <zmcbride@shopkeep.com>
2014-06-04 20:42:27 +0100

    Initial Commit

---
+++ SwiftTest2Tests/SwiftTest2Tests.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftTest2Tests.swift
+//  SwiftTest2Tests
+//
+//  Created by Zack McBride on 04/06/2014.
+//  Copyright (c) 2014 Zack McBride. All rights reserved.
+//
+
+import XCTest
+
+class SwiftTest2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
